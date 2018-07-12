0ca27acc930cb4ccbf5f248d959d2e9d369caa45
Rodrigo Soldi <rodrigosoldi@icloud.com>
2018-07-12 14:46:01 -0300

    Initial Commit

---
+++ RSPagedViewController/RSPagedViewController.h
@@ -0,0 +1,19 @@
+//
+//  RSPagedViewController.h
+//  RSPagedViewController
+//
+//  Created by Rodrigo Soldi on 12/07/18.
+//  Copyright © 2018 Soldi Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSPagedViewController.
+FOUNDATION_EXPORT double RSPagedViewControllerVersionNumber;
+
+//! Project version string for RSPagedViewController.
+FOUNDATION_EXPORT const unsigned char RSPagedViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSPagedViewController/PublicHeader.h>
+
+
